77bc84f722cd9b7c14d32ba5359f03c935231d8f
leixiao <2399520027@qq.com>
2018-11-22 22:46:37 +0800

    update

---
+++ taotao-manage/taotao-web/src/test/java/com/taotao/controller/TestPageHelper.java
@@ -0,0 +1,28 @@
+package com.taotao.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class TestPageHelper {
+    @Test
+    public void testPageHelper(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:/spring/applicationContext-*.xml");
+        TbItemMapper mapper = context.getBean(TbItemMapper.class);
+        TbItemExample example = new TbItemExample();
+        PageHelper.startPage(1,10);
+        List<TbItem> list = mapper.selectByExample(example);
+        for (TbItem item : list){
+            System.out.println(item.getTitle());
+        }
+        PageInfo<TbItem> info = new PageInfo<>(list);
+        System.out.println(info.getTotal());
+    }
+}
